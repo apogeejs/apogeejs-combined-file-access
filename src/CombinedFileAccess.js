902eb9250e65e0159d286db3e30c71eac7bf986f
sutter-dave <sutter@intransix.com>
2021-02-22 17:49:40 -0800

    added initial content - from web app formerly

---
+++ src/CombinedFileAccess.js
@@ -0,0 +1,80 @@
+import ClipboardFileSourceGenerator from "/apogeejs-combined-file-access/src/clipboard/ClipboardFileSource.js";
+import OneDriveFileSourceGenerator from "/apogeejs-combined-file-access/src/onedrive/OneDriveFileSource.js";
+import CombinedAccessDialog from "/apogeejs-combined-file-access/src/CombinedFileAccessDialog.js";
+import * as fileAccessConstants from "/apogeejs-combined-file-access/src/fileAccessConstants.js";
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+export default class CombinedFileAccess {
+    
+    //========================================
+    // Public
+    //========================================
+    
+    constructor() {
+        this.defaultSourceId = ClipboardFileSourceGenerator.getSourceId();
+        this.sourceGeneratorList = [ClipboardFileSourceGenerator,OneDriveFileSourceGenerator];
+    }
+    
+    /**
+     * This method returns true if the workspace has an existing file to which 
+     * is can be saved without opening a save dialog. 
+     */
+    directSaveOk(fileMetadata) {
+        let sourceGenerator = this._getSourceGenerator(fileMetadata,this.sourceGeneratorList)
+        return ((sourceGenerator)&&(sourceGenerator.directSaveOk(fileMetadata))); 
+    }
+    
+    /**
+     * This method opens a file, including dispalying a dialog
+     * to select the file.
+     */
+    openFile(onOpen) {
+        let dialogObject = new CombinedAccessDialog(fileAccessConstants.OPEN_ACTION,null,null,this.sourceGeneratorList,onOpen);
+        dialogObject.showDialog();
+    }
+
+    /** This  method shows a save dialog and saves the file. */
+    saveFileAs(fileMetadata,data,onSave) {
+        let dialogObject = new CombinedAccessDialog(fileAccessConstants.SAVE_ACTION,fileMetadata,data,this.sourceGeneratorList,onSave);
+        dialogObject.showDialog();     
+    }
+
+    /** 
+     * This method saves a file to the give location. 
+     */
+    saveFile(fileMetadata,data,onSave) {
+        //make sure we can save
+        let sourceGenerator = this._getSourceGenerator(fileMetadata,this.sourceGeneratorList)
+        if((sourceGenerator)&&(sourceGenerator.directSaveOk(fileMetadata))) {
+            let source = sourceGenerator.getInstance(fileAccessConstants.SAVE_ACTION,fileMetadata,data,onSave);
+            source.updateFile();
+        }
+        else {
+            //if we can't save, revert to save as
+            this.saveFileAs(fileMetadata,data,onSave);
+        }
+    }
+
+    //============================
+    // Private Functions
+    //============================
+    
+    _getSourceGenerator(fileMetadata,generatorList) {
+        let sourceId;
+        if((fileMetadata)&&(fileMetadata.source)) {
+            sourceId = fileMetadata.source;
+        }
+        else {
+            return null;
+        }
+        return generatorList.find( sourceGenerator => sourceGenerator.getSourceId() == sourceId );
+    }
+
+
+}
+
+
